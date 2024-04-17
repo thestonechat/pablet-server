9ed969c1be5b6f1c89d1ae1c6e5d945060dcac41
unknown <tguyy33@gmail.com>
2024-04-18 01:43:26 +0400

    works

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import asyncio
 import websockets
 import pyautogui
 import struct
+import socket
 
 pyautogui.FAILSAFE = False
 
-IP = "192.168.100.4"
+
+def get_local_ip(interface="Wi-Fi"):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.255.255.255", 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
+
+
+IP = get_local_ip()
 PORT = 3244
 
 
@@ -21,17 +36,20 @@ async def echo(websocket, path):
 
         ints = struct.unpack(
             "<2h", message
-        )  # Unpack 2 signed 16-bit integers (little-endian)
+        )  # Unpack 2 signed 16-bit integers (4 bytes)
 
-        # Now you have the integers
+        # floating points are multiplied by 100 and turned into ints so we can keep the 2 decimal precision while lowering the sent data size for extra efficiency
         x = ints[0] / 100
         y = ints[1] / 100
+
         print(f"Cursor position: {x}, {y}")
         pyautogui.moveRel(x, y, _pause=False)
 
 
 def main():
     start_server = websockets.serve(echo, IP, PORT)
+
+    print(f"Server started at ws://{IP}:{PORT}")
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
